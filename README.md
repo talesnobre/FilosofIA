9c4e400da71e67f9b467b85047b4251d6e7192b1
raphael720 <raphaelleite720@gmail.com>
2023-06-10 12:17:18 -0300

    criação do requirements.txt e arquivo.py

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Projeto_PLN
+
+## Instalação das dependências
+Certifique-se de ter instalado todas as dependências necessárias antes de executar o arquivo Python. Você pode fazer isso executando o seguinte comando no terminal:
+
+```bash
+pip install -r requirements.txt
+```
+
+Este comando irá ler o arquivo requirements.txt e instalar todas as dependências listadas nele.
+
+## Gerando o Dataset
+Após a instalação das dependências, você pode executar o arquivo Python usando o seguinte comando:
+
+```bash
+python arquivo.py
+```
+
+Certifique-se de estar no diretório correto onde o arquivo.py está localizado antes de executar o comando.
---
+++ create_dataset.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+import os
+import pandas as pd
+
+data = "dados"
+directory = "livros_filosofia"  # Diretório dos arquivos
+
+dict_df = {"autor": [], "titulo": [], "texto": []}
+
+for i, filename in enumerate(os.listdir(directory)):
+    if filename.endswith(".txt"):
+        with open(os.path.join(directory, filename), "r") as file:
+            texto = file.read()
+            autor, titulo = filename.split("-")[0], filename.split("-")[1].split(".txt")[0]
+
+            texto_split = texto.split("*** START")[1].split("*** END")[0]
+            texto = texto_split[30:]
+
+            dict_df["autor"].append(autor)
+            dict_df["titulo"].append(titulo)
+            dict_df["texto"].append(texto)
+
+df = pd.DataFrame(dict_df)
+
+df.to_csv(os.path.join(data, "livros_filosofia.csv"), index=False)
